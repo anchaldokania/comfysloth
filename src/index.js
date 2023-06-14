50042bfc7853c71d932e1ce93dfb63f14464f9f4
anchaldokania <62883566+anchaldokania@users.noreply.github.com>
2023-06-14 19:27:11 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-djuzk6f316yst2aw.us.auth0.com"
+    clientId="5Qxe2dJIPpKTXhbvtsXRcGgvc4l5JVFJ"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import CheckOut from "./CheckoutPage";
+import Error from "./ErrorPage";
+import Home from "./HomePage";
+import PrivateRoute from "./PrivateRoute";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import AuthWrapper from "./AuthWrapper";
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  CheckOut,
+  PrivateRoute,
+  AuthWrapper,
+};
